a8ed73427aecc504e6f524e33513e642fad1c5b9
Zhihang Zhang <462143015zzh@gmail.com>
2023-11-13 15:12:12 -0800

    add zig treesitter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { 'c', 'cpp', 'rust' },
+  ensure_installed = { 'c', 'cpp', 'rust', 'zig' },
   auto_install = true,
   textobjects = {
       select = {
